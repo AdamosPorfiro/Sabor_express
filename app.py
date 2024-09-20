23619e926c3068534f5f6c45320a33bdc466ee3a
AdamosPorfiro <adamos.contato@gmail.com>
2024-09-19 23:32:59 -0300

    Projeto_restaurante_sabor_express

---
+++ app.py
@@ -0,0 +1,17 @@
+'''
+Print exibi uma informação na tela para o usuario;
+Para executar utilizando windows, abrimos o terminal e escrevemos: python app.py
+'''
+#print('Hello,World')
+print('Sabor express\n') #\n Pula uma linha
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = input('Escolha uma opção: ')#input = Recebemos informações do usuario (É possivel passar informação'exibir') criamos uma variavel para armazenar a opção escolhida pelo usuario
+print('Você escolheu a opção', opcao_escolhida) #Podemos exibir uma mensagem e juntar a informação passada pelo usuario em um print
+
+
+
