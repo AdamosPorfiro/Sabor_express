e93f7c93c83e4f27f56d26dc19d50ab37c50330d
AdamosPorfiro <adamos.contato@gmail.com>
2024-09-19 23:48:02 -0300

    Usando_fstring_e_fsymbols

--- app.py
+++ app.py
@@ -3,7 +3,19 @@ Print exibi uma informação na tela para o usuario;
 Para executar utilizando windows, abrimos o terminal e escrevemos: python app.py
 '''
 #print('Hello,World')
-print('Sabor express\n') #\n Pula uma linha
+print("""
+      ░██████╗░█████╗░██████╗░░█████╗░██████╗░
+      ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗
+      ╚█████╗░███████║██████╦╝██║░░██║██████╔╝
+      ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗
+      ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║
+      ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝
+███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+█████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░\n""") #\n Pula uma linha
 
 print('1. Cadastrar restaurante')
 print('2. Listar restaurante')
@@ -11,7 +23,7 @@ print('3. Ativar restaurante')
 print('4. Sair\n')
 
 opcao_escolhida = input('Escolha uma opção: ')#input = Recebemos informações do usuario (É possivel passar informação'exibir') criamos uma variavel para armazenar a opção escolhida pelo usuario
-print('Você escolheu a opção', opcao_escolhida) #Podemos exibir uma mensagem e juntar a informação passada pelo usuario em um print
+print(f'Você escolheu a opção {opcao_escolhida}') #Podemos exibir uma mensagem e juntar a informação passada pelo usuario em um print usando fstring = interpolação de string, usando 'f' no inicio da informação e '{}' para invocar a variavel ou função a ser exibida.
 
 
 
