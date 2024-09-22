83579ce95b7d5df3254fc37ce64bc9eb574dffcd
portaria1 - Portaria - Cirúrgica Fernandes <portaria1@cfernandes.com.br>
2024-09-22 08:22:41 -0300

    Informções_da_aula

--- app.py
+++ app.py
@@ -15,12 +15,14 @@ print("""
 █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
 ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
 ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
-╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░\n""") #\n Pula uma linha
+╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
+""") #Podemos fazer uma quebra de linha utlizado aspas triplas, usamos dependendo da situação
+#https://fsymbols.com/pt/ - Alteração da fonte
 
 print('1. Cadastrar restaurante')
 print('2. Listar restaurante')
 print('3. Ativar restaurante')
-print('4. Sair\n')
+print('4. Sair\n') #\n Pula uma linha
 
 opcao_escolhida = input('Escolha uma opção: ')#input = Recebemos informações do usuario (É possivel passar informação'exibir') criamos uma variavel para armazenar a opção escolhida pelo usuario
 print(f'Você escolheu a opção {opcao_escolhida}') #Podemos exibir uma mensagem e juntar a informação passada pelo usuario em um print usando fstring = interpolação de string, usando 'f' no inicio da informação e '{}' para invocar a variavel ou função a ser exibida.
