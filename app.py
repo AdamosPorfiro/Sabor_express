b3270b2b678f4ce2fbaa247333e0ff7eb246bb19
portaria1 - Portaria - Cirúrgica Fernandes <portaria1@cfernandes.com.br>
2024-09-22 15:58:39 -0300

    Sintaxe_match

--- app.py
+++ app.py
@@ -28,8 +28,6 @@ def exibir_opcoes():
     print('3. Ativar restaurante')
     print('4. Sair\n') #\n Pula uma linha
 
-
-
 def escolher_opcao():
     opcao_escolhida = int(input('Escolha uma opção: '))
     # opcao_escolhida = int(opcao_escolhida) #input = Recebemos informações do usuario (É possivel passar informação'exibir') criamos uma variavel para armazenar a opção escolhida pelo usuario
@@ -37,8 +35,19 @@ def escolher_opcao():
 
     print(f'Você escolheu a opção {opcao_escolhida}') #Podemos exibir uma mensagem e juntar a informação passada pelo usuario em um print usando fstring = interpolação de string, usando 'f' no inicio da informação e '{}' para invocar a variavel ou função a ser exibida.
     #se opcao_escolhida for igual a 1{codigos}
-    
-    if opcao_escolhida == 1:
+    match opcao_escolhida:
+        case 1:
+            print('Cadastrar restaurante')
+        case 2:
+            print('Listar restaurantes')
+        case 3:
+            print('Ativar restaurante')
+        case 4:
+            finalizar_app()
+        case _:
+            print('Opção inválida')
+          
+    """if opcao_escolhida == 1:
         print('Cadastrar restaurante')
     #se não se
     elif opcao_escolhida == 2:
@@ -48,7 +57,7 @@ def escolher_opcao():
     #Se não
     else:
     #     print('Encerrando o programa')
-        finalizar_app()
+        finalizar_app()"""
 
 def finalizar_app(): # def = Definição, cria uma função
      system('cls') #no windows
