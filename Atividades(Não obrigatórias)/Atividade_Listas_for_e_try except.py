a07b228ec80d967ea2b719494da44110642eee23
portaria1 - Portaria - Cirúrgica Fernandes <portaria1@cfernandes.com.br>
2024-09-26 11:46:49 -0300

    Atividade_lista_for_e_try_except

--- Atividades(Não obrigatórias)/Atividade_Listas_for_e_try except.py
+++ Atividades(Não obrigatórias)/Atividade_Listas_for_e_try except.py
@@ -10,26 +10,24 @@ lista_comano_de_nascimento_e_ano_atual = [1993, 2024]
 
 # 2 - Crie uma lista e utilize um loop for para percorrer todos os elementos da lista.
 
-# for numero in lista_de_numeros:
-#     if  type(numero) == int:
-#         print(f' → {numero}', end=' ')
-#     else:
-#         print(' → Terminou')    
+for numero in lista_de_numeros:
+        print(f' → {numero}', end=' ')   
 
 # 3 - Utilize um loop for para calcular a soma dos números ímpares de 1 a 10.
-# soma = 0
-# for numero in lista_de_numeros:
-#     if numero % 2 != 0:
-#         soma += numero
-# print(f'A soma dos número ímpares é: {soma}')
+soma = 0
+for numero in lista_de_numeros:
+    if numero % 2 != 0:
+        soma += numero
+print(f'\nA soma dos número ímpares é: {soma}')
 
 # 4 - Utilize um loop for para imprimir os números de 1 a 10 em ordem decrescente.
 for i in reversed(lista_de_numeros):
-    if type(i) == int:
         print(f' → {i}', end=' ')
 
 # 5 - Solicite ao usuário um número e, em seguida, utilize um loop for para imprimir a tabuada desse número, indo de 1 a 10.
-
+numero_escolhido = int(input('\n\nInforme um número para receber sua tabuada: '))
+for i in range(1,11):
+      print(f'{i:2} x {numero_escolhido} = {i*numero:3}')
 # 6 - Crie uma lista de números e utilize um loop for para calcular a soma de todos os elementos. Utilize um bloco try-except para lidar com possíveis exceções.
 
 # 7 - Construa um código que calcule a média dos valores em uma lista. Utilize um bloco try-except para lidar com a divisão por zero, caso a lista esteja vazia.
