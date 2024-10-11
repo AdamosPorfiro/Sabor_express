723c628e3fbe50eb925c4b3979c00a0f739f1f61
portaria1 - Portaria - Cirúrgica Fernandes <portaria1@cfernandes.com.br>
2024-10-10 23:04:42 -0300

    Atividade_OO

--- Atividades(Não obrigatórias)/Atividade_OO.py
+++ Atividades(Não obrigatórias)/Atividade_OO.py
@@ -1,6 +1,29 @@
 # 1.Atribua o valor 'Italiana' ao atributo categoria da instância restaurante_praca da classe Restaurante.
+
+class Restaurante:
+    nome = ''
+    categoria = ''
+    ativo = False
+
+restaurante_praca = Restaurante()
+restaurante_praca.nome = 'Praça'
+restaurante_praca.categoria = 'Italiana'
+
+restaurantes = [restaurante_praca]
+
+print(vars(restaurantes[0]))
+
 # 2.Acesse o valor do atributo nome da instância restaurante_praca da classe Restaurante.
+
+print(f'Categoria: {restaurantes[0].categoria}')
+
 # 3.Verifique o valor inicial do atributo ativo para a instância restaurante_praca e exiba uma mensagem informando se o restaurante está ativo ou inativo.
+
+if restaurantes[0].ativo == False:
+    print('Restaurante Praça está INATIVO!')
+else:
+    print('Restaurante Praça está ATIVO!')
+    
 # 4.Acesse o valor do atributo de classe categoria diretamente da classe Restaurante e armazene em uma variável chamada categoria.
 # 5.Altere o valor do atributo nome para 'Bistrô'.
 # 6.Crie uma nova instância da classe Restaurante chamada restaurante_pizza com o nome 'Pizza Place' e categoria 'Fast Food'.
