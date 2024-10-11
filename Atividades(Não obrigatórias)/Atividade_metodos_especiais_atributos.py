ad39a7fcb7f393cc62eb54190701f835b0578a91
portaria1 - Portaria - Cirúrgica Fernandes <portaria1@cfernandes.com.br>
2024-10-11 04:33:24 -0300

    Atividade_metodos_atributos

--- Atividades(Não obrigatórias)/Atividade_metodos_especiais_atributos.py
+++ Atividades(Não obrigatórias)/Atividade_metodos_especiais_atributos.py
@@ -4,22 +4,6 @@
 
 # 3Modifique a classe Restaurante adicionando um construtor que aceita nome e categoria como parâmetros e inicia ativo como False por padrão Crie uma instância utilizando o construtor
 
-class Restaurante():
-    
-    def __init__(,nome,categoria,cardapio,preco):
-        nome = nome
-        categoria = categoria
-        ativo = False
-        cardapio = cardapio
-        preco = preco
-
-    def __str__():
-        return f'Nome: {nome}\nCategoria: {categoria}\nCardapio: {cardapio}\nPreço: R$ {preco} | {ativo}'
-
-restaurante = Restaurante('Slice Tennis', 'Comida Típica', 'Arroz c/ feijão e bife acebolado', 2350)
-
-print(restaurante)
-
 # 4Adicione um método especial __str__ à classe Restaurante para que, ao imprimir uma instância, seja exibida uma mensagem formatada com o nome e a categoria Exiba essa mensagem para uma instância de restaurante
 
 
