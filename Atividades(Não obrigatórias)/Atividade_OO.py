ee72b9ced243c0a056aaf846b06dd633d087f323
portaria1 - Portaria - Cirúrgica Fernandes <portaria1@cfernandes.com.br>
2024-10-10 23:35:48 -0300

    Atividade_OO

--- Atividades(Não obrigatórias)/Atividade_OO.py
+++ Atividades(Não obrigatórias)/Atividade_OO.py
@@ -23,10 +23,47 @@ if restaurantes[0].ativo == False:
     print('Restaurante Praça está INATIVO!')
 else:
     print('Restaurante Praça está ATIVO!')
-    
+
 # 4.Acesse o valor do atributo de classe categoria diretamente da classe Restaurante e armazene em uma variável chamada categoria.
+
+categoria = Restaurante().categoria
+print(categoria) # Vazio
+
 # 5.Altere o valor do atributo nome para 'Bistrô'.
+
+restaurantes[0].nome = 'Bistrô'
+print(f'Nome do restaurante: {restaurantes[0].nome}')
+
+#ou
+
+restaurante_praca.nome = 'BISTRÔ'
+print(f'Nome do restaurante: {restaurante_praca.nome}')
+
+
 # 6.Crie uma nova instância da classe Restaurante chamada restaurante_pizza com o nome 'Pizza Place' e categoria 'Fast Food'.
+
+restaurante_pizza = Restaurante()
+restaurante_pizza.nome = 'Pizza Place'
+restaurante_pizza.categoria = 'Fast Food'
+restaurantes.append(restaurante_pizza)
+
+print(vars(restaurantes[1]))
+
+
 # 7.Verifique se a categoria da instância restaurante_pizza é 'Fast Food'.
+
+if restaurantes[1].categoria == 'Fast Food':
+    print(f'Sim, O {restaurante_pizza.nome} está na categoria de Fast Food')
+else:
+    print(f'Não, O {restaurante_pizza.nome} não está na categoria de Fast Food')
+
 # 8.Mude o estado da instância restaurante_pizza para ativo.
+
+restaurante_pizza.ativo = True
+if restaurante_pizza.ativo == True:
+    print('Ativo')
+else:
+    print('Inativo')
+
 # 9.Imprima no console o nome e a categoria da instância restaurante_praca.
+print(f'Nome do restaurante: {restaurantes[0].nome}',f'Categoria do restaurante: {restaurante_praca.categoria}', sep='\n')
