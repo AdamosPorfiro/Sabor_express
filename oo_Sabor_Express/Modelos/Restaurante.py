443c1cc91bda7307b4238d69f2dabafcfd0cbf2f
portaria1 - Portaria - Cirúrgica Fernandes <portaria1@cfernandes.com.br>
2024-10-17 02:32:45 -0300

    Metodo de classe

--- oo_Sabor_Express/Modelos/Restaurante.py
+++ oo_Sabor_Express/Modelos/Restaurante.py
@@ -1,26 +1,31 @@
 class Restaurante: # Classe + Atributos
     restaurantes = []
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
+        self._nome = nome.title()
+        self._categoria = categoria.title()
         self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
     
-    def listar_restaurantes():
+    @classmethod
+    def listar_restaurantes(cls):
         print(f'{'Nome'.ljust(18)} | {'Categoria'.ljust(18)} | {'Status'}')
         print('-' * 50)
-        for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome.ljust(18)} | {restaurante.categoria.ljust(18)} | {restaurante.ativo.ljust(18)}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(18)} | {restaurante._categoria.ljust(18)} | {restaurante.ativo.ljust(18)}')
     
     @property #Um decorator(decorador) do python: Tem a capacidade de pegar um atributo, no nosso caso o ativo.
     def ativo(self):
         return '⌧' if self._ativo else '☐'
     
-restaurante_praca = Restaurante('Praça','Gourmet')
-restaurante_pizza = Restaurante('Pizza Express', 'Italiana')
+    def alternar_estado(self):
+        self._ativo = not self._ativo
+    
+restaurante_praca = Restaurante('praça','gourmet')
+restaurante_praca.alternar_estado()
+restaurante_pizza = Restaurante('pizza Express', 'italiana')
 
 # print(dir(restaurante_praca)) #dir = Exibe todas os atributos, métodos e propriedades de um self.
 #vars = Mostra um dicionarios dos atributos e metodos, que foi os atributos passados no código anterior
