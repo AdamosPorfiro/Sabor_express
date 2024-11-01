6e77d160934fe58ad72ebda4510256a2edec693c
AdamosPorfiro <adamos.contato@gmail.com>
2024-10-31 23:01:59 -0300

    Aplicação_avaliação

--- oo_Sabor_Express/Modelos/Restaurante.py
+++ oo_Sabor_Express/Modelos/Restaurante.py
@@ -1,9 +1,12 @@
+from Avaliação import Avaliacao
+
 class Restaurante: # Classe + Atributos
     restaurantes = []
     def __init__(self, nome, categoria):
         self._nome = nome.title()
         self._categoria = categoria.title()
         self._ativo = False
+        self._avaliacao = []
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
@@ -11,10 +14,10 @@ class Restaurante: # Classe + Atributos
     
     @classmethod
     def listar_restaurantes(cls):
-        print(f'{'Nome'.ljust(18)} | {'Categoria'.ljust(18)} | {'Status'}')
+        print(f'{'Nome'.ljust(18)} | {'Categoria'.ljust(18)} | {'Avaliações'.ljust(18)} | {'Status'}')
         print('-' * 50)
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome.ljust(18)} | {restaurante._categoria.ljust(18)} | {restaurante.ativo.ljust(18)}')
+            print(f'{restaurante._nome.ljust(18)} | {restaurante._categoria.ljust(18)} | {str(restaurante.media_avaliacao).ljust(18)} | {restaurante.ativo}')
     
     @property #Um decorator(decorador) do python: Tem a capacidade de pegar um atributo, no nosso caso o ativo.
     def ativo(self):
@@ -23,7 +26,18 @@ class Restaurante: # Classe + Atributos
     def alternar_estado(self):
         self._ativo = not self._ativo
     
+    def receber_avaliacao(self, cliente, nota):
+        avaliacao = Avaliacao(cliente, nota)
+        self._avaliacao.append(avaliacao)
+
+    @property
+    def media_avaliacao(self):
+        if not self._avaliacao:
+            return 0 
+        soma_das_nontas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        quantidade_de_notas = len(self._avaliacao)
+        media = round(soma_das_nontas / quantidade_de_notas, 1)
+        return media
 # print(dir(restaurante_praca)) #dir = Exibe todas os atributos, métodos e propriedades de um self.
 #vars = Mostra um dicionarios dos atributos e metodos, que foi os atributos passados no código anterior
-Restaurante.listar_restaurantes()
 
--- oo_Sabor_Express/Modelos/app.py
+++ oo_Sabor_Express/Modelos/app.py
@@ -1,10 +1,12 @@
 from Restaurante import Restaurante
 
 restaurante_praca = Restaurante('Praça', 'Gourmet',)
-restaurante_mexicano = Restaurante('Mexican food', 'Mexicana')
-restaurante_mexicano.alternar_estado()
-restaurante_japones = Restaurante('Japa', 'Japonesa')
-
+# restaurante_mexicano = Restaurante('Mexican food', 'Mexicana')
+# restaurante_mexicano.alternar_estado()
+# restaurante_japones = Restaurante('Japa', 'Japonesa')
+restaurante_praca.receber_avaliacao('Gui', 10)
+restaurante_praca.receber_avaliacao('Laís', 8)
+restaurante_praca.receber_avaliacao('Emy', 5)
 
 def main():
     Restaurante.listar_restaurantes()
