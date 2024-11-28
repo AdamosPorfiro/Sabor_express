4e56684c5d3a8830f5b6663d9f406689383e6b8c
AdamosPorfiro <adamos.contato@gmail.com>
2024-11-27 21:52:44 -0300

    Criando_arquivos_com_python

--- app_venv.py
+++ app_venv.py
@@ -4,6 +4,7 @@ vamos importar a biblioteca request que vai permitir que possamos acessar esses 
 """
 
 import requests #Biblioteca de requisições e respostas
+import json
 
 url = 'https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json' #Esse arquivo é muito comumente usado em programação, chamad JSON
 
@@ -32,8 +33,21 @@ if response.status_code == 200:
             'price' : item['price'],
             'description' : item['description']
         })
+
+
 else:
     print(f'O erro foi {response.status_code}')
 
-print(dados_restaurante['McDonald’s'])
+'''
+Essa interação cria arquivos json separando e filtrando o arquivo com base nos nomes dos restaurantes
+'''
 
+for nome_do_restaurante, dados in dados_restaurante.items():
+    '''
+    Temos a chave que será o nome do restaurante e valores que são os dados, 1° variável é o nome do nosso arquivo, with open permite manipular os arquivos também dentro da aplicação, passamos 2 informações que é o nome do nosso arquivo e w = write(escrever) e vamos nomear  o nome desse arquivo com o nome arquivo_restaurante usamos 'as'
+
+    Para criar o aquivo importamos a biblioteca .json e usamos a função dump que vai converter o dicionário em um arquivo string, passamos 3 informações, o valor de for (dados) | arquivo_restaurante que estamos trabalhando e por último a identação para ficar mais legível 
+    '''
+    nome_do_arquivo = f'{nome_do_restaurante}.json'
+    with open(nome_do_arquivo, 'w') as arquivo_restaurante:
+        json.dump(dados, arquivo_restaurante, indent=4)
